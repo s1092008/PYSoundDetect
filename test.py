036c0a8a053bcdeeceaf17cf6a5b9240e0a4e652
DESKTOP-2F5S139\USER <s1092008class@gmail.com>
2024-03-12 16:32:53 +0800

    可運作 但可能PY time不夠 有時候串口會有占用問題 想辦法做出可以循環接收的方法

--- test.py
+++ test.py
@@ -1,3 +1,2 @@
-x=input('pls enter temperature °C :')
-a=(int(x)*9/5)+32
-print(a,'°F')
+
+
--- testmic.py
+++ testmic.py
@@ -1,15 +1,17 @@
 import pyaudio
 import wave
 import speech_recognition as sr
+import serial
 
 chunk = 1024                     # 記錄聲音的樣本區塊大小
 sample_format = pyaudio.paInt16  # 樣本格式，可使用 paFloat32、paInt32、paInt24、paInt16、paInt8、paUInt8、paCustomFormat
 channels = 2                     # 聲道數量
 fs = 44100                       # 取樣頻率，常見值為 44100 ( CD )、48000 ( DVD )、22050、24000、12000 和 11025。
 seconds = 5                      # 錄音秒數
-filename = "oxxostudio.wav"      # 錄音檔名
+filename = "teststudio.wav"      # 錄音檔名
+ser = serial.Serial('COM3', 9600)
 
-p = pyaudio.PyAudio()            # 建立 pyaudio 物件
+p = pyaudio.PyAudio()            #      建立 pyaudio 物件
 
 print("開始錄音...")
 
@@ -28,22 +30,23 @@ p.terminate()
 
 print('錄音結束...')
 
-# 寫入音訊到 WAV 檔
-wf = wave.open(filename, 'wb')
-wf.setnchannels(channels)
-wf.setsampwidth(p.get_sample_size(sample_format))
-wf.setframerate(fs)
-wf.writeframes(b''.join(frames))
-wf.close()
-
+# 將原始音訊資料儲存到 WAV 檔案中
+with wave.open(filename, 'wb') as wf:
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
 
 # 使用 SpeechRecognition 來辨識錄音中的文字
 recognizer = sr.Recognizer()
-audio = recognizer.listen(stream, phrase_time_limit=7)
-text = recognizer.recognize_google(audio, language='zh-TW')  # 识别中文
-print(text)
-
 with sr.AudioFile(filename) as source:
     audio_data = recognizer.record(source)
-    text = recognizer.recognize_google(audio_data, language='zh-TW')  # 识别中文
-    print("他說: ", text)
+    text = recognizer.recognize_google(audio_data, language='zh-TW')
+
+Text=text.split()
+print(Text)
+x='沈'
+if x in text:
+    ser.write(b'go')
+else:
+    ser.write(b'stop')
