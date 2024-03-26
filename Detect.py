199fdf92bb6cfc2a7c7b09f539c650467885f35f
DESKTOP-2F5S139\USER <s1092008class@gmail.com>
2024-03-26 19:27:41 +0800

    可以運作 只要放入store.cvs

--- Detect.py
+++ Detect.py
@@ -4,16 +4,21 @@
 import speech_recognition as sr
 import serial
 import csv
+import time
 
 
-ser=serial.Serial('COM3',9600)
-key="都你在講"
+# ser=serial.Serial('COM3',9600)
+try:
+    ser = serial.Serial('COM3', 9600)
+except serial.SerialException as e:
+    print(f"串列通訊連接失敗: {e}")
+    exit()
 
 def Voice_To_Text(duration=10): 
     r = sr.Recognizer()
     with sr.Microphone() as source:
         # print(type(source))
-        # print("請開始說話:")
+        print("請開始說話:")
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source, phrase_time_limit=duration)
     try:
@@ -31,12 +36,18 @@ store_text=[]
 
 def Scoring(text):
     # filtered_keywords=list(filter(lambda text: any(text),a))
-    if text in store_text:
-        print("成功")
-        ser.write(b'go')
-    else:
-        ser.write(b'stop')
-        
+    # if text in store_text:
+    #     print(text)
+    #     ser.write(b'go')
+    # else:
+    #     ser.write(b'stop')
+    for keyword in store_text:
+        if keyword in text:
+            print("成功")
+            ser.write(b'go')
+            return
+    return
+
 
 with open(csv_path, newline='', encoding='utf-8') as csvfile:
    # 讀取 CSV 檔案內容
@@ -45,23 +56,16 @@ with open(csv_path, newline='', encoding='utf-8') as csvfile:
   # 以迴圈輸出每一列
   for row in rows:
     store_text.extend(row)
-    for i in row:
-        Scoring(i)
+    # for i in row:
+    #     Scoring(i)  
 
 
-while word:
-    Text=Voice_To_Text(10)
-    word=Text.split()
-    for word in store_text:
-        Scoring(word)
-    
-# csv_path = "output.csv"
-# header = ["Text"]
-# with open(csv_path, "w", newline="", encoding="utf-8") as csv_file:
-#     csv_writer = csv.writer(csv_file)
-#     csv_writer.writerow(header)
-#     for i in range(3):
-#         Text = Voice_To_Text(10)
-#         csv_writer.writerow([Text])
-#         print(Text)
-# print("已將語音轉換結果寫入 output.csv 文件")
+Text=Voice_To_Text(10)
+# words=Text.split()
+# for word in words:
+#     Scoring(word)
+Scoring(Text)
+
+
+
+ser.close()
